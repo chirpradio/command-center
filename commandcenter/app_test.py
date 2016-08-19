c31bf6447f7b373ecbcc79e161dc406907887344
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-19 16:35:36 -0500

    Add another test to check that the "See new artists" page is basically rendered correctly

--- commandcenter/app_test.py
+++ commandcenter/app_test.py
@@ -20,3 +20,13 @@ class TestMyApp(AsyncHTTPTestCase):
         self.assertRegexpMatches(
             body,
             r'<a href="/new-artists/" class="[^"]+">See new artists</a>')
+
+    def test_new_artists_page(self):
+        response = self.fetch('/new-artists/')
+        self.assertEqual(response.code, 200)
+        body = response.body.decode('utf-8')
+
+        self.assertIn('See new artists</h1>', body)
+        self.assertIn(
+            'This command will show you what artists are (supposedly) not yet in the database.',
+            body)
